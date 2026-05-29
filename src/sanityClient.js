9339a37874389f524bd8712cf5a5c5b75d42f318
techman177 <your-email@example.com>
2026-05-29 19:32:58 -0400

    actualizacion completa

--- src/sanityClient.js
+++ src/sanityClient.js
@@ -2,7 +2,7 @@ import { createClient } from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = createClient({
-  projectId: 'TU_PROJECT_ID', // <--- PON TU ID AQUÍ
+  projectId: '20vh16ty', // <--- PON TU ID AQUÍ
   dataset: 'production',
   useCdn: true, 
   apiVersion: '2024-05-29', 
