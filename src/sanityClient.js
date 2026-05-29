bc88e984b248d7a8ace3e4e35da71f20102d52bf
techman177 <your-email@example.com>
2026-05-29 16:53:51 -0400

    "Agregando sanityClient"

---
+++ src/sanityClient.js
@@ -0,0 +1,12 @@
+import { createClient } from '@sanity/client';
+import imageUrlBuilder from '@sanity/image-url';
+
+export const client = createClient({
+  projectId: 'TU_PROJECT_ID', // <--- PON TU ID AQUÍ
+  dataset: 'production',
+  useCdn: true, 
+  apiVersion: '2024-05-29', 
+});
+
+const builder = imageUrlBuilder(client);
+export const urlFor = (source) => builder.image(source);
